fdb547305d9081207c1be8fc3983cf4061c05bb3
Jan Krupiński <jk.krupinski@gmail.com>
2022-06-17 21:11:45 +0200

    Project: Add project structure

---
+++ src/app/App.tsx
@@ -0,0 +1,10 @@
+import AppProviders from './AppProviders';
+import AppRoutes from './AppRoutes';
+
+const App = () => (
+  <AppProviders>
+    <AppRoutes />
+  </AppProviders>
+);
+
+export default App;
