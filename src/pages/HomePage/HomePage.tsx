918700c8e7806573e4c2dbec2d27f65db6de4d3b
Jan Krupiński <jk.krupinski@gmail.com>
2022-06-18 11:57:36 +0200

    HomePage: Hide logo

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -4,7 +4,7 @@ import Button from 'components/Button';
 const HomePage = () => {
   return (
     <div className="flex flex-row gap-12 mt-24">
-      <img src={logo} alt="logo" className="w-96 mt-auto" />
+      <img src={logo} alt="logo" className="w-96 mt-auto hidden md:block" />
       <div className="mt-auto mb-6">
         <h1 className="text-6xl font-bold mb-8">Witaj w Memory&nbsp;Twitter!</h1>
         <h2 className="text-2xl mb-4">Wygląda na to, że nie jesteś jeszcze zalogowany</h2>
