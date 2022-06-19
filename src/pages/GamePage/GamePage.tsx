d15ce2b7fde904df6a0597daefca4dc739f4afeb
Jan Krupiński <jk.krupinski@gmail.com>
2022-06-19 20:15:02 +0200

    Game: Add game page

--- src/pages/GamePage/GamePage.tsx
+++ src/pages/GamePage/GamePage.tsx
@@ -1,36 +1,11 @@
-import { useState } from 'react';
+import { useParams } from 'react-router-dom';
 
-import Button from 'components/Button';
-import Radio, { RadioGroup } from 'components/Radio';
-import { Level } from 'model';
+import { PathParams } from './model';
 
 const GamePage = () => {
-  const [level, setLevel] = useState<Level>(Level.Easy);
+  const { level } = useParams<PathParams>();
 
-  return (
-    <div>
-      <h1 className="text-center text-2xl font-bold">Wybierz poziom</h1>
-      <div className="flex flex-col gap-4 items-center m-6">
-        <RadioGroup value={level}>
-          <Radio value={Level.Easy} onCheck={() => setLevel(Level.Easy)}>
-            Łatwy
-          </Radio>
-          <Radio value={Level.Medium} onCheck={() => setLevel(Level.Medium)}>
-            Średni
-          </Radio>
-          <Radio value={Level.Hard} onCheck={() => setLevel(Level.Hard)}>
-            Trudny
-          </Radio>
-          <Radio value={Level.Legendary} onCheck={() => setLevel(Level.Legendary)}>
-            Legendarny
-          </Radio>
-        </RadioGroup>
-        <Button size="large" onClick={() => {}}>
-          Graj!
-        </Button>
-      </div>
-    </div>
-  );
+  return <div>{level}</div>;
 };
 
 export default GamePage;
