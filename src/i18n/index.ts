29f56dedb468a0366fee7eac87ccd3a2f556828c
Jan Krupiński <jk.krupinski@gmail.com>
2022-07-21 22:21:48 +0200

    I18n: Add language detection

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,18 +1,22 @@
 import { initReactI18next } from 'react-i18next';
 
 import i18next from 'i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
 import Backend from 'i18next-http-backend';
 
 i18next
   .use(Backend)
+  .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    lng: 'en',
     ns: 'menu',
     debug: true,
     backend: {
       loadPath: 'src/i18n/translations/{{lng}}/{{ns}}.json',
       crossDomain: true,
+    },
+    detection: {
+      order: ['navigator'],
     },
   });
 
