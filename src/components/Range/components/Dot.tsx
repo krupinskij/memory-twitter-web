9753d54b9644511438d9d82380ab1470083609e8
Jan Krupiński <jk.krupinski@gmail.com>
2022-07-23 13:24:01 +0200

    Settings: Bigger range

--- src/components/Range/components/Dot.tsx
+++ src/components/Range/components/Dot.tsx
@@ -9,14 +9,19 @@ type DotProps<T extends RangeValue> = {
 
 function Dot<T extends RangeValue>({ label, value, selected, onSelect }: DotProps<T>) {
   return (
-    <div className="relative cursor-pointer" onClick={() => onSelect(value)}>
+    <div className="relative cursor-pointer">
       <div
         className={`
           ${selected ? 'scale-125 ring-8' : ''} hover:ring-8 ring-primary/20 
           h-3 w-3 rounded-full bg-primary relative z-20 shadow-lg
         `}
+        onClick={() => onSelect(value)}
       />
       <div className="absolute top-6 left-1/2 -translate-x-1/2 whitespace-nowrap">{label}</div>
+      <div
+        className="absolute cursor-pointer top-1/2 left-1/2 w-[50px] h-[70px] -translate-x-1/2 -translate-y-[35%]"
+        onClick={() => onSelect(value)}
+      />
     </div>
   );
 }
