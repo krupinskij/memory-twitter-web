9966dbc299fe925f4ad17286a7aee67ad76c295b
Jan Krupiński <jk.krupinski@gmail.com>
2022-07-27 18:13:53 +0200

    Errors: Check if level is valid

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,16 +3,24 @@ import React from 'react';
 type ButtonProps = {
   variant?: 'contained' | 'outlined';
   size?: 'small' | 'medium' | 'large';
+  disabled?: boolean;
   onClick: () => void;
   children: React.ReactNode;
 };
 
-const Button = ({ variant = 'contained', size = 'medium', children, onClick }: ButtonProps) => {
+const Button = ({
+  variant = 'contained',
+  size = 'medium',
+  disabled,
+  children,
+  onClick,
+}: ButtonProps) => {
   return (
     <button
       className={`rounded-full font-bold border-2 border-primary py-2
         ${variant} contained:bg-primary contained:text-textContained outlined:bg-background
         ${size}  medium:px-4 large:px-20 large:text-lg
+        ${disabled ? 'pointer-events-none border-borderSecondary contained:bg-borderSecondary' : ''}
       `}
       onClick={onClick}
     >
--- src/pages/GameOptionsPage/GameOptionsPage.tsx
+++ src/pages/GameOptionsPage/GameOptionsPage.tsx
@@ -1,19 +1,29 @@
 import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import { useQuery } from 'react-query';
 import { useLocation, useNavigate } from 'react-router';
 
+import API, { QUERY } from 'api';
 import Button from 'components/Button';
+import { Spinner } from 'components/Loading';
 import Radio, { RadioGroup } from 'components/Radio';
 import { Level } from 'model';
 import { useNotification } from 'providers/NotificationProvider';
 
 const GameOptionsPage = () => {
-  const [level, setLevel] = useState<Level>(Level.Easy);
+  const [level, setLevel] = useState<Level | null>(null);
   const navigate = useNavigate();
   const location = useLocation();
 
   const { t } = useTranslation();
   const { send } = useNotification();
+
+  const { data: availableLevels = new Set(), isLoading } = useQuery(QUERY.LEVELS, API.getLevels, {
+    select: (data) => {
+      if (!level && data.length > 0) setLevel(data[0]);
+      return new Set(data);
+    },
+  });
 
   useEffect(() => {
     const error = (location.state as any)?.error;
@@ -23,18 +33,27 @@ const GameOptionsPage = () => {
     }
   }, []);
 
+  if (isLoading) {
+    return <Spinner />;
+  }
+
   return (
     <div>
       <h1 className="text-center text-2xl font-bold">{t('gameoptions:title')}</h1>
       <div className="flex flex-col gap-4 items-center m-6">
         <RadioGroup value={level}>
           {Object.values(Level).map((level) => (
-            <Radio key={level} value={level} onCheck={() => setLevel(level)}>
+            <Radio
+              key={level}
+              value={level}
+              disabled={!availableLevels.has(level)}
+              onCheck={() => setLevel(level)}
+            >
               {t(`gameoptions:${level}`)}
             </Radio>
           ))}
         </RadioGroup>
-        <Button size="large" onClick={() => navigate(`/game/${level}`)}>
+        <Button size="large" disabled={!level} onClick={() => navigate(`/game/${level}`)}>
           {t('gameoptions:play')}
         </Button>
       </div>
