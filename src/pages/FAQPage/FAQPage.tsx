4747461e58ba6fc5aec27df8817b88fcd2833d84
Jan Krupiński <jk.krupinski@gmail.com>
2022-07-27 20:31:06 +0200

    FAQ: Show text on page

--- src/pages/FAQPage/FAQPage.tsx
+++ src/pages/FAQPage/FAQPage.tsx
@@ -1,11 +1,45 @@
 import { useTranslation } from 'react-i18next';
 
 const FAQPage = () => {
-  const { t } = useTranslation();
+  const { i18n, t } = useTranslation();
   return (
     <div className="mx-8">
-      <h1 className="text-2xl font-bold mb-3">{t('display:title')}</h1>
-      <p className="mb-12">{t('display:subtitle')}</p>
+      <div className="pb-12">
+        <h1 className="text-2xl font-bold mb-3">{t('faq:blocked_levels.q')}</h1>
+        <p>{t('faq:blocked_levels.a1')}</p>
+        <p>{t('faq:blocked_levels.a2')}</p>
+      </div>
+      <div className="pb-12">
+        <h1 className="text-2xl font-bold mb-3">{t('faq:time.q')}</h1>
+        <p>{t('faq:time.a')}</p>
+      </div>
+      <div className="pb-12">
+        <h1 className="text-2xl font-bold mb-3">{t('faq:clicks.q')}</h1>
+        <p>{t('faq:clicks.a1')}</p>
+        <p>{t('faq:clicks.a2')}</p>
+      </div>
+      <div className="pb-12">
+        <h1 className="text-2xl font-bold mb-3">{t('faq:stop_following.q')}</h1>
+        <p>{t('faq:stop_following.a')}</p>
+      </div>
+      <div className="pb-12">
+        <h1 className="text-2xl font-bold mb-3">{t('faq:following_change.q')}</h1>
+        <p>{t('faq:following_change.a')}</p>
+      </div>
+      <div className="pb-12">
+        <h1 className="text-2xl font-bold mb-3">{t('faq:me_change.q')}</h1>
+        <p>{t('faq:me_change.a')}</p>
+      </div>
+      {i18n.language === 'pl' && (
+        <div className="pb-12">
+          <h1 className="text-2xl font-bold mb-3">{t('faq:language.q')}</h1>
+          <p>{t('faq:language.a')}</p>
+        </div>
+      )}
+      <div className="pb-12">
+        <h1 className="text-2xl font-bold mb-3">{t('faq:logout.q')}</h1>
+        <p>{t('faq:logout.a')}</p>
+      </div>
     </div>
   );
 };
